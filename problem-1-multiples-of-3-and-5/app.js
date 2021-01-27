5b40689d0d240ddc586cfc743bfae26e9bc45d39
Mrs. Krabs <47456460+MasterKrab@users.noreply.github.com>
2021-01-27 20:37:06 -0300

    add function

--- problem-1-multiples-of-3-and-5/app.js
+++ problem-1-multiples-of-3-and-5/app.js
@@ -1,7 +1,12 @@
-const limit = 1000
-let result = 0
 
-for(let i = 1; i < limit; i++){
-   if(i % 3 === 0 || i % 5 === 0) result += i
+const calculate = (limit) =>{
+   let result = 0
+
+   for(let i = 1; i < limit; i++){
+      if(i % 3 === 0 || i % 5 === 0) result += i
+   }
+
+   return result
 }
-console.log(result)
+
+console.log(calculate(1000))
