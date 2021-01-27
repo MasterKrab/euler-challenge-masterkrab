816918479635941b7c3d3d4cd3d0cf255fc17c21
Mrs. Krabs <47456460+MasterKrab@users.noreply.github.com>
2021-01-27 20:39:55 -0300

    add function

--- problem-2-even-fibonacci-numbers/app.js
+++ problem-2-even-fibonacci-numbers/app.js
@@ -1,14 +1,19 @@
-let x = 0
-let y = 1
-let sum = 0
-let result = x + y
-x = result
 
-while(result <=  4000000){
-   result = x + y
-   if(result % 2 === 0) sum += result
-   y = x
+const calculateFibbonaci = limit =>{
+   let x = 0
+   let y = 1
+   let sum = 0
+   let result = x + y
    x = result
+   
+   while(result <= limit){
+      result = x + y
+      if(result % 2 === 0) sum += result
+      y = x
+      x = result
+   }
+   return `The sum is: ${sum}`
 }
 
-console.log(`The sum is: ${sum}`)
+console.log(calculateFibbonaci(4000000))
+
