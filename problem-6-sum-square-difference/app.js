879e5e5fc6fd3b2678b965bf986f08ef05190aac
Mrs. Krabs <47456460+MasterKrab@users.noreply.github.com>
2021-02-07 22:53:46 -0300

    update

---
+++ problem-6-sum-square-difference/app.js
@@ -0,0 +1,24 @@
+// The sum of the squares of the first ten natural numbers is,  1^2 + 2^2 + ... + 10^2 = 385
+
+// The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)^2 = 55^2 = 3025
+
+// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
+
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+const differenceSum = limit =>{
+
+   let squareSum = 0
+   let sum = 0
+
+   for(let i = 1; i <= limit; i++){
+      
+      squareSum += Math.pow(i, 2)
+      sum += i
+      
+   }
+
+   return (Math.pow(sum, 2)) - squareSum
+}
+
+console.log(differenceSum(100))
