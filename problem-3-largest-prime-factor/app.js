c37f62df4e6d34771a6dd05c7d9b591639465865
Mrs. Krabs <47456460+MasterKrab@users.noreply.github.com>
2021-02-15 23:11:33 -0300

    Better solution

--- problem-3-largest-prime-factor/app.js
+++ problem-3-largest-prime-factor/app.js
@@ -2,20 +2,23 @@
 
 //What is the largest prime factor of the number 600851475143 ?
 
-
-const isPrime = value =>{
-   for(let i = 2; i < value; i++){
-      if(value % i !== 0) {
-         return true
-     }  
-   }
+const isPrime = number =>{
+   for(let i = 2; i < number; i++)
+      if(number % i === 0) return false
+   return true
 }
 
 const largestPrime = number =>{
-   for(let i = number - 1; i > 0; i--){
-      if(number % i === 0 && i !== number)
-         if(isPrime(i)) return i
+
+   let prime = 2
+
+   while(number !== 1){
+      if(isPrime(prime) && number % prime === 0)
+         number = number / prime
+      else
+         prime++
    }
+   return prime
 }
 
 console.log(largestPrime(600851475143))
